f67010f8831b298644c38d1443734a4e0f2e72b2
eddy.de.boer <eddy.de.boer@medicore.nl>
2017-12-28 10:29:55 +0100

    Answer: step 26, refactoring

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -7,7 +7,9 @@ use PHPUnit\Framework\TestCase;
 
 class GameTest extends TestCase
 {
-    /** @var \BowlingGameKata\Game */
+    /**
+     * @var \BowlingGameKata\Game
+     */
     private $game;
 
     protected function setUp()
@@ -15,19 +17,34 @@ class GameTest extends TestCase
         $this->game = new Game();
     }
 
+    /**
+     * @testdox Test case where every ball ends up in the gutter:no points!
+     */
     public function testGutterGame()
     {
-        for ($i = 0; $i < 20; ++$i) {
-            $this->game->roll(0);
-        }
+        $this->rollMany(20, 0);
         $this->assertSame(0, $this->game->getScore());
     }
 
+    /**
+     * @testdox Test if you throw only 1's.
+     */
     public function testAllOnes()
     {
-        for ($i = 0; $i < 20; ++$i) {
-            $this->game->roll(1);
-        }
+        $this->rollMany(20, 1);
         $this->assertSame(20, $this->game->getScore());
+    }
+
+    /**
+     * Throw $times, with $pins as result.
+     *
+     * @param int $times
+     * @param int $pins
+     */
+    private function rollMany($times, $pins)
+    {
+        for ($i = 0; $i < $times; ++$i) {
+            $this->game->roll($pins);
+        }
     }
 }
