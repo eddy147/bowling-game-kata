035bc68b0a390ff253f62a0bd2de23088482a73b
eddy.de.boer <eddy.de.boer@medicore.nl>
2017-12-28 09:57:13 +0100

    Skeleton

---
+++ src/Game.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BowlingGameKata;
+
+class Game
+{
+
+}
---
+++ tests/GameTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BowlingGameKata\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase
+{
+
+}
