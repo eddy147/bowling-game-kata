031c44edb1cefafd61b5643aade93798db5aa1a7
eddy.de.boer <eddy.de.boer@medicore.nl>
2017-12-28 10:15:17 +0100

    Answer: step 19, added score

--- src/Game.php
+++ src/Game.php
@@ -4,5 +4,13 @@ namespace BowlingGameKata;
 
 class Game
 {
+    public function roll($pins)
+    {
 
+    }
+
+    public function score()
+    {
+        return 0;
+    }
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -10,5 +10,9 @@ class GameTest extends TestCase
     public function testGutterGame()
     {
         $game = new Game();
+        for ($i = 0; $i < 20; ++$i) {
+            $game->roll(0);
+        }
+        $this->assertSame(0, $game->score());
     }
 }
